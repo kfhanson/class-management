1c8a5dd83057dc9d45c0108c01035d7bcf6fda5b
kfhanson <kfhanson.2508@gmail.com>
2024-12-10 23:03:39 +0700

    View and assets

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -5,8 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Attendance Management System</title>
+    <link rel="shortcut icon" href="{{ URL::asset('assets/images/') }}">
+    <link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('assets/css/metismenu.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('plugins/sweetalert.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('plugins/RWD-Table-Patterns/dist/css/rwd-table.min.css') }}" rel="stylesheet" type="text/css" media="screen">
+    <link href="{{ URL::asset('plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
-    
+
 </body>
 </html>
