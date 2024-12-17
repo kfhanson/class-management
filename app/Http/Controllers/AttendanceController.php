c91f5b2333b4f7caa918e9ff588aacf3c9cf89d1
kfhanson <kfhanson.2508@gmail.com>
2024-12-17 14:56:12 +0700

    web

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
 use App\Models\Attendance;
+use App\Models\Employee;
+use App\Models\Latetime;
 use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
@@ -11,5 +14,16 @@ class AttendanceController extends Controller
         return view('admin.attendance')->with(['attendances' => Attendance::all()]);
     }
 
-    
+    public static function lateTimeDevice($att_dateTime, Employee $employee)
+    {
+        $attendance_time = new DateTime($att_dateTime);
+        $checkin = new DateTime($employee->schedules->first()->time_in);
+        $difference = $checkin->diff($attendance_time)->format('%H:%I:%S');
+
+        $latetime = new Latetime();
+        $latetime->emp_id = $employee->id;
+        $latetime->duration = $difference;
+        $latetime->latetime_date = date('Y-m-d', strtotime($att_dateTime));
+        $latetime->save();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\LeaveController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\CheckController;
+use App\Http\Controllers\BiometricDeviceController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +21,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { return view('welcome'); })->name('welcome');
-// Route::get('attended/{user_id}', [AttendanceController::class] )
+
+Route::get('attended/{user_id}', [AttendanceController::class, 'attended'])->name('attended');
+Route::get('attended-before/{user_id}', [AttendanceController::class, 'attendedBefore'])->name('attendedBefore');
+
+Auth::routes(['register' => false, 'reset' => false]);
+
+Route::group(['middleware' => ['auth', 'Role'], 'roles' => ['admin']], function () {
+    // Employees Resource
+    Route::resource('/employees', EmployeeController::class);
+
+    // Attendance
+    Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance');
+    Route::get('/latetime', [AttendanceController::class, 'indexLatetime'])->name('indexLatetime');
+
+    // Leave
+    Route::get('/leave', [LeaveController::class, 'index'])->name('leave');
+    Route::get('/overtime', [LeaveController::class, 'indexOvertime'])->name('indexOvertime');
+
+    // Admin Dashboard
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+
+    // Schedule Resource
+    Route::resource('/schedule', ScheduleController::class);
+
+    // Check Controller
+    Route::get('/check', [CheckController::class, 'index'])->name('check');
+    Route::get('/sheet-report', [CheckController::class, 'sheetReport'])->name('sheet-report');
+    Route::post('check-store', [CheckController::class, 'CheckStore'])->name('check_store');
+});
