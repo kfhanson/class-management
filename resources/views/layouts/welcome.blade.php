c2ee0848be07493c3c1538193d2083c20f4e8c1e
HammerBone <jarviswinata@gmail.com>
2024-12-20 01:14:11 +0700

    refactorL: import attendanceFront.css

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Attendance Management System</title>
+    <link href="{{ URL::asset('assets/css/attendanceFront.css') }}" rel="stylesheet" type="text/css" />
     @include('layouts.link')
 </head>
 <body class="pb-0" style="background: #2a3142;">
