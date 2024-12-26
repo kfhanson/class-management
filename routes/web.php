f29b4f3f91057acaa4c1047f8954a69ddcf1b2ed
HammerBone <jarviswinata@gmail.com>
2024-12-27 00:20:22 +0700

    fix: just bug fix and not done yet. I'm lazy to write the commit details

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\CheckController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\LeaveController;
 use App\Http\Controllers\ProfileController;
@@ -56,5 +57,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
 
 require __DIR__.'/auth.php';
