57336bc73179c0a00b37714326ecf39331f0d062
HammerBone <109730807+HammerBone@users.noreply.github.com>
2024-12-27 18:11:29 +0700

    Update welcome.blade.php

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Attendance Management System</title>
-    <link href="{{ URL::asset('assets/css/attendanceFront.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('/assets/css/attendanceFront.css') }}" rel="stylesheet" type="text/css" />
     @include('layouts.link')
 </head>
 <body class="pb-0" style="background: #2a3142;">
     @yield('content')
     @include('layouts.footer-script')
     @include('includes.flash')
-    <script src="{{ URL::asset('assets/js/attendanceFront.js') }}"></script>
+    <script src="{{ URL::asset('/assets/js/attendanceFront.js') }}"></script>
 </body>
 </html>
